a5831e0a076970f012664647a25534cbfc453e25
Eyup Kayak <eyup.kayak@dgpays.com>
2021-12-10 19:28:42 +0300

    Entities add user

---
+++ Entities/Abstract/IDisplayEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{
+    internal interface IDisplayEntity
+    {
+        int DisplayOrder { get; set; }
+        bool IsDisplay { get; set; }
+    }
+}
---
+++ Entities/Concrete/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.BaseEntities
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity, IUpdateEntity
+    {
+        public int UpdateUserId { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public int CreatedUserId { get; set ; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
