d4671e0e9c72f586226154829d9cc951964d9ed2
Eyup Kayak <eyup.kayak@dgpays.com>
2021-12-10 20:39:35 +0300

    created context

---
+++ DataAccess/Concrete/Contexts/ECommercceProjectWebApiDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Contexts
+{
+    public class ECommercceProjectWebApiDbContext:DbContext
+    {
+        public ECommercceProjectWebApiDbContext(DbContextOptions<ECommercceProjectWebApiDbContext> options):base(options)
+        {
+
+        }
+        public ECommercceProjectWebApiDbContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
